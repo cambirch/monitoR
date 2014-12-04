76e90747cb17d4528fc88703e1af3b5ad5327dd8
Cam Birch <cam@maintenanceconnection.ca>
2014-12-03 20:20:57 -0700

    .

---
+++ monitoR.Service/monitoR.Service/Config.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace monitoR.Service
+{
+    partial class Config
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtScript = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(384, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run Script";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtScript
+            // 
+            this.txtScript.AcceptsReturn = true;
+            this.txtScript.AcceptsTab = true;
+            this.txtScript.Font = new System.Drawing.Font("Consolas", 8F);
+            this.txtScript.Location = new System.Drawing.Point(4, 4);
+            this.txtScript.MaxLength = 999999999;
+            this.txtScript.Multiline = true;
+            this.txtScript.Name = "txtScript";
+            this.txtScript.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtScript.Size = new System.Drawing.Size(456, 180);
+            this.txtScript.TabIndex = 1;
+            this.txtScript.Text = "get-date";
+            this.txtScript.WordWrap = false;
+            // 
+            // txtResult
+            // 
+            this.txtResult.AcceptsReturn = true;
+            this.txtResult.AcceptsTab = true;
+            this.txtResult.Font = new System.Drawing.Font("Consolas", 8F);
+            this.txtResult.Location = new System.Drawing.Point(4, 212);
+            this.txtResult.MaxLength = 999999999;
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(704, 332);
+            this.txtResult.TabIndex = 2;
+            this.txtResult.WordWrap = false;
+            // 
+            // Config
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 546);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtScript);
+            this.Controls.Add(this.button1);
+            this.Name = "Config";
+            this.Text = "Config";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtScript;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
---
+++ monitoR.Service/monitoR.Service/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Topshelf;
+
+namespace monitoR.Service
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            //HostFactory.Run(x => {
+            //                    x.Service<App>();
+            //                    x.RunAsLocalSystem();
+            //                    x.StartAutomaticallyDelayed();
+
+            //                    x.SetDescription("Monitors a windows server for critical events and sends notifications.");
+            //                    x.SetDisplayName("MonitoR");
+            //                    x.SetServiceName("MonitoR");
+
+            //                    x.EnableServiceRecovery(r => { r.RestartService(1); });
+            //                });
+
+            Application.Run(new Config());
+
+        }
+    }
+}
